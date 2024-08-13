752c4f4c3372370e721aebbfe771844cb26a16da
LMJ0900 <alswodnjswo@naver.com>
2024-08-13 14:02:50 +0900

    깃확인

--- frontend/resome/src/components/home/propile/propile.tsx
+++ frontend/resome/src/components/home/propile/propile.tsx
@@ -10,7 +10,7 @@ export default function Propile(){
           <div className="flex flex-row ml-[10vw] mt-[3rem] gap-[3rem] ">
             <Circle1 title='My Resome' bg='bg-blue-200' />
             <Circle1 title='My Project' bg='bg-pink-200' />
-            <h2 className="text-2xl font-semibold md:text-3xl lg:text-5xl">안녕하세요 <br /><p className=" font-light md:text-xl lg:text-3xl">끊임없이 도전하고 노력하는 <br /> 프론트엔드 개발자 이민재입니다. <br />부족한 부분을 찾고 개선하려 노력하고 있습니다 <br />잘 부탁드립니다!</p> </h2>
+            <h2 className="text-2xl font-semibold md:text-3xl lg:text-5xl">안녕하세요 깃푸쉬확인 <br /><p className=" font-light md:text-xl lg:text-3xl">끊임없이 도전하고 노력하는 <br /> 프론트엔드 개발자 이민재입니다. <br />부족한 부분을 찾고 개선하려 노력하고 있습니다 <br />잘 부탁드립니다!</p> </h2>
           </div>
         </>
     )
