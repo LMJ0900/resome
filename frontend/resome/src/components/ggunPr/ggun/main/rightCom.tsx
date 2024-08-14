ed2eca0969718248399dbdf4460dc2305733c4ff
LMJ0900 <alswodnjswo@naver.com>
2024-08-14 15:07:00 +0900

    dns

--- frontend/resome/src/components/ggunPr/ggun/main/rightCom.tsx
+++ frontend/resome/src/components/ggunPr/ggun/main/rightCom.tsx
@@ -1,10 +1,28 @@
+'use client'
 
-export default function RightCom(){
-    return(
+import { useState } from "react";
+import BarChart from "./com/BarChart";
+import { } from 'chart.js/auto';
+import LineChart from "./com/LineChart";
+import { KosdaqData } from "./data/Kosdaq";
+
+
+export default function KospiCom() {
+
+    const [kosdaqData, setKosdaqData] = useState({
+        labels: KosdaqData.map((data) => data.year),
+        datasets: [{
+            label: "KOSDAQ",
+            data: KosdaqData.map((data) => data.result),
+            backgroundColor: ["blue"],
+        }],
+        borderColor: "black",
+        borderWidth: 2,
+
+    })
+    return (
         <div className=" w-[25vw] h-[30vh]">
-            <div className="flex justify-center">
-            <img className="w-[22.5vw] h-[30vh]" src="https://cdn.getnews.co.kr/news/photo/202107/538156_224818_2335.png" alt="코스닥" />
-            </div>
+            <LineChart chartData={kosdaqData}></LineChart>
         </div>
     )
 }
